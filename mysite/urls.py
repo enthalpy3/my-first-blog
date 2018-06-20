20515c1401cde32928c3691d2918955d161743cb
DS <enthalpy3@gmail.com>
2018-06-20 15:54:29 +0900

    숙제

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,7 +5,5 @@ from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/login/$', views.login, name='login'),
-    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page':'/'}),
     url(r'', include('blog.urls', namespace='blog')),
 ]
